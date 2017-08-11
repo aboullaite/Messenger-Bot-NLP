ee033e84853532525edb2261f2f541a0cc3db69f
Mohammed Aboullaite <aboullaite.mohammed@gmail.com>
2017-08-11 16:55:20 +0100

    Fix treshold to 0.8

--- src/main/java/me/aboullaite/nlpmessengerbot/service/MessageHandler.java
+++ src/main/java/me/aboullaite/nlpmessengerbot/service/MessageHandler.java
@@ -21,11 +21,12 @@ public class MessageHandler {
     private final String GREETINGS_MSG = "greetings";
     private final String LOCATION_MSG = "location";
     private final String GENERIC_MSG = "Greeting from Fero Weld Hero";
+    private final double TRESHOLD = 0.8;
     public  String handleMessage(Map<String,List<Map<String,Object>>> entities){
         if(EntityMap.entityExist(GREETINGS_MSG, entities))
-         return  EntityMap.confidence(GREETINGS_MSG, entities) > 0.8 ? "Hello Sir!" : GENERIC_MSG;
+         return  EntityMap.confidence(GREETINGS_MSG, entities) > TRESHOLD ? "Hello Sir!" : GENERIC_MSG;
         if(EntityMap.entityExist(LOCATION_MSG, entities))
-            return  EntityMap.confidence(LOCATION_MSG, entities) > 0.8 ?  Weather.weather(EntityMap.value(LOCATION_MSG, entities), omwtoken) : GENERIC_MSG;
+            return  EntityMap.confidence(LOCATION_MSG, entities) > TRESHOLD ?  Weather.weather(EntityMap.value(LOCATION_MSG, entities), omwtoken) : GENERIC_MSG;
 
         return  GENERIC_MSG;
 
